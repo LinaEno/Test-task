7527a07f310c12e355accc8c2ab9530782d119aa
Lina Budkova <linabudkova35@gmail.com>
2023-05-07 23:20:38 +0300

    change

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,10 @@ const App = () => {
   return (
     <>
       <Helmet>
-        <meta property="og:image" content="/public/house3.jpg" />
+        <meta
+          property="og:image"
+          content="https://images.unsplash.com/photo-1570129477492-45c003edd2be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+        />
         <meta name="og:email" content="linabudkova35@gmail.com" />
         <meta
           property="og:description"
