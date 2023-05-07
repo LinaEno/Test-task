0e643fd7fde5b18a171c5612c8a946131146e60d
Lina Budkova <linabudkova35@gmail.com>
2023-05-07 23:15:59 +0300

    add helmet

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { Helmet } from "react-helmet";
+
 import Navbar from "./components/Navbar/Navbar";
 import HeroSection from "./components/HeroSection/HeroSection";
 import TourSection from "./components/TourSection/TourSection";
@@ -10,16 +12,34 @@ import { Container } from "./components/HeroSection/HeroSection.styled";
 
 const App = () => {
   return (
-    <Container>
-      <Navbar />
-      <HeroSection />
-      <FeaturedSection />
-      <TourSection />
-      <ReviewSection />
-      <ArticleSection />
-      <SubscribeSection />
-      <Footer />
-    </Container>
+    <>
+      <Helmet>
+        <meta property="og:image" content="/public/house3.jpg" />
+        <meta name="og:email" content="linabudkova35@gmail.com" />
+        <meta
+          property="og:description"
+          content="Find the place to live your dreams
+          easily here"
+        />
+        <meta property="og:title" content="Hounter" />
+        <meta name="title" content="Hounter" />
+        <meta
+          name="description"
+          content="Find the place to live your dreams
+          easily here"
+        />
+      </Helmet>
+      <Container>
+        <Navbar />
+        <HeroSection />
+        <FeaturedSection />
+        <TourSection />
+        <ReviewSection />
+        <ArticleSection />
+        <SubscribeSection />
+        <Footer />
+      </Container>
+    </>
   );
 };
 
